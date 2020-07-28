2a3a241b3b0221a484396be3463a265659d9f90b
HaoyuanJi <haoyuanji@gmail.com>
2020-07-27 17:28:28 -0700

    Create china_data.js

---
+++ china_data.js
@@ -0,0 +1,8 @@
+        var subDataChina = [
+            {colorIndex: 0, value: 283040, label: "Submarine"},
+            {colorIndex: 6, value: 85500, label: "Corvette"},           
+            {colorIndex: 12, value: 294700, label: "Amphibious Warfare Ships"}, 
+            {colorIndex: 14, value: 137500, label: "Aircraft Carrier"},
+            {colorIndex: 10, value: 166590, label: "Frigate"},
+            {colorIndex: 8, value: 229320, label: "Destroyer"}, 
+        ];
