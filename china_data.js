bc91b697635c10c73eb8012f5f9fe569b3da61ba
HaoyuanJi <haoyuanji@gmail.com>
2021-04-17 11:10:11 -0700

    Update china_data.js

--- china_data.js
+++ china_data.js
@@ -98,6 +98,7 @@
 
         var subDataCNDe055 = [
             {colorIndex: 0, value: 13000, label: "Nanchang(南昌)"}, 
+            {colorIndex: 5, value: 13000, label: "Lhasa(拉萨)"}, 
         ];
 
         var subDataCNDe052_c = [
@@ -125,16 +126,18 @@
             {colorIndex: 5, value: 7500, label: "Taiyuan(太原)"}, 
             {colorIndex: 10, value: 7500, label: "Hohhot(呼和浩特)"}, 
             {colorIndex: 14, value: 7500, label: "Chengdu(成都)"},
+            {colorIndex: 9, value: 7500, label: "Qiqihar(齐齐哈尔)"},
         ];
 
         var subDataCNDe052_d3 = [
             {colorIndex: 0, value: 7500, label: "Zibo(淄博)"},
+            {colorIndex: 5, value: 7500, label: "Tangshan(唐山)"},
         ];
 
         var subDataCNDe052_d = [
             {colorIndex: 0, value: 60000, childData: subDataCNDe052_d1, label: "Batch I"},
-            {colorIndex: 5, value: 30000, childData: subDataCNDe052_d2, label: "Batch II"}, 
-            {colorIndex: 10, value: 7500, childData: subDataCNDe052_d3, label: "Batch III"}, 
+            {colorIndex: 5, value: 37700, childData: subDataCNDe052_d2, label: "Batch II"}, 
+            {colorIndex: 10, value: 15000, childData: subDataCNDe052_d3, label: "Batch III"}, 
         ];
 
         var subDataCNDeSov = [
@@ -164,11 +167,11 @@
         ];
 
         var subDataCNDestroyer = [
-            {colorIndex: 0, value: 13000, childData: subDataCNDe055, label: "Type 055"},             
+            {colorIndex: 0, value: 26000, childData: subDataCNDe055, label: "Type 055"},             
             {colorIndex: 6, value: 42000, childData: subDataCNDe052_c, label: "Type 052C"},
             {colorIndex: 8, value: 14200, childData: subDataCNDe051_c, label: "Type 051C"},          
             {colorIndex: 2, value: 6100, childData: subDataCNDe051_b, label: "Type 051B"}, 
-            {colorIndex: 3, value: 97500, childData: subDataCNDe052_d, label: "Type 052D"},
+            {colorIndex: 3, value: 112500, childData: subDataCNDe052_d, label: "Type 052D"},
             {colorIndex: 10, value: 13000, childData: subDataCNDe052_b, label: "Type 052B"},
             {colorIndex: 14, value: 33920, childData: subDataCNDeSov, label: "Sovremenny Class"},
             {colorIndex: 5, value: 9600, childData: subDataCNDe052, label: "Type 052"},
@@ -301,5 +304,5 @@
             {colorIndex: 12, value: 294700, label: "Amphibious Warfare Ships(两栖作战舰)"}, 
             {colorIndex: 14, value: 137500, childData: subDataCNAC, label: "Aircraft Carrier(航空母舰)"},
             {colorIndex: 10, value: 162590, childData: subDataCNFrigate, label: "Frigate(驱护舰)"},
-            {colorIndex: 8, value: 229320, childData: subDataCNDestroyer, label: "Destroyer(驱逐舰)"}, 
+            {colorIndex: 8, value: 257300, childData: subDataCNDestroyer, label: "Destroyer(驱逐舰)"}, 
         ];
